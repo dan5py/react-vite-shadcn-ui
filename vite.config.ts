3f29b7219eef922f3846242aac39c6d4f14d38ab
Amju <86785660+amjed-ali-k@users.noreply.github.com>
2023-11-29 23:05:20 +0530

    fix(config): path not defined error (#3)
    * Update vite.config.ts
    
    fixed 'path not defined' error
    
    * fix: add @types/node

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': resolve(__dirname, './src'),
     },
   },
 });
