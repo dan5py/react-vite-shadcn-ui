980aabb623e755a89860d7bf172d2f0e959e9e6b
dan5py <git@dan5py.com>
2025-02-08 23:37:19 +0100

    feat: bump dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -19,6 +19,10 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
     },
   }
 );
